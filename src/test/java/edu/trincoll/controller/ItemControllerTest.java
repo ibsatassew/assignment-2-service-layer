ef796bda05af7a5e6dbfca4dd25a9d1a86c6bc9f
Kelly Asefa <kasefa@trincoll.edu>
2025-09-24 00:20:09 -0400

    Update ItemControllerTest.java

--- src/test/java/edu/trincoll/controller/ItemControllerTest.java
+++ src/test/java/edu/trincoll/controller/ItemControllerTest.java
@@ -18,6 +18,7 @@ import java.util.Optional;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
