e1d7ae440fdf730aad5a01fabcfe1c39fa1f4d75
Kelly Asefa <kasefa@trincoll.edu>
2025-09-24 00:17:54 -0400

    Update ItemRepository.java

--- src/main/java/edu/trincoll/repository/ItemRepository.java
+++ src/main/java/edu/trincoll/repository/ItemRepository.java
@@ -5,7 +5,7 @@ import edu.trincoll.model.Item;
 import java.util.List;
 import java.util.Optional;
 
-public interface ItemRepository extends Repository<Item, Long> {
+public interface ItemRepository {
 
     Item save(Item item);
 
